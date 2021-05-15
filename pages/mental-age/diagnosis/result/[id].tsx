7f0c2b434b0e7874a81fca49104450ba88ea1593
tresagua0123 <gentlekai10@gmail.com>
2021-05-16 01:47:06 +0900

    deploy to see if image is successfully shown.

--- pages/mental-age/diagnosis/result/[id].tsx
+++ pages/mental-age/diagnosis/result/[id].tsx
@@ -38,19 +38,26 @@ export default function Result({mentalAgeData}) {
     <Wrapper>
         <Head 
         description={mentalAgeData.description}
-        image={mentalAgeData.id > 60 ? OLD_MAN_IMAGE : BABY_IMAGE}
+        // image={mentalAgeData.id > 60 ? OLD_MAN_IMAGE : BABY_IMAGE}
+        // url={`https://next-quiz-maker.vercel.app/mental-age/diagnosis/result/${mentalAgeData.id}`}
+        // TODO: 正しくシェアの時にイメージを表示させる。
+        image={"https://dotup.org/uploda/dotup.org2474715.png"}
         title={mentalAgeData.title}
         />
         <p>Result</p>
         <p>{mentalAgeData.description}</p>
         <img src={mentalAgeData.id > 60 ? OLD_MAN_IMAGE : BABY_IMAGE}/>
-        <img src={"https://dotup.org/uploda/dotup.org2474715.png"} />
+        {/* <img src={"https://dotup.org/uploda/dotup.org2474715.png"} /> */}
         <p>この結果をシェアする</p>
         <Link href={"/mental-age"}>もどる</Link>
         <ShareButtonsWrapper>
-        <TwitterShareButton title={"精神年齢"} url={`https://next-quiz-maker.vercel.app/mental-age/diagnosis/result/${mentalAgeData.id}`} via={"kaimaru31"}>
+        <TwitterShareButton title={"精神年齢アドバイザー"} url={`https://next-quiz-maker.vercel.app/mental-age/diagnosis/result/${mentalAgeData.id}`} via={"kaimaru31"}>
             <TwitterIcon size={31} round />
         </TwitterShareButton>
+        {/*なぜか下の方だとうまくシェアできる。 */}
+           {/* <TwitterShareButton title={"精神年齢アドバイザー"} url={`http://c769af6a8b6e.ngrok.io/mental-age/diagnosis/result/${mentalAgeData.id}`} via={"kaimaru31"}>
+            <TwitterIcon size={31} round />
+        </TwitterShareButton> */}
         {/* <FacebookShareButton title={"精神年齢"} url={"http://708d4b7795cb.ngrok.io/mental-age/diagnosis/result/2"} >
             <FacebookIcon size={31} round />
         </FacebookShareButton> */}
